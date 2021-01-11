8c7906d8fdd0b07ec9c868b88c5c98179d9c82f5
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-11 00:03:50 -0500

    Training

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,11 +2,15 @@ export default function Header() {
   const $header = document.createElement("header");
   $header.classList.add("border-bottom");
   $header.innerHTML = `
-    <nav class="nav justify-content-end">
-      <span class="nav-link disabled" tabindex="-1">Built by <a class="nav-link" href="https://www.javierandresgp.com"
-          target="_blank"
-          rel="noopener noreferrer">Javier Andrés GP</a>.&nbsp;&nbsp;Found a bug or have an idea? Contact me</span>
-    </nav>
+<nav class="nav justify-content-end">
+  <span class="nav-link disabled " tabindex="-1">
+    Built by
+  </span>
+  <a class="nav-link" href="https://www.javierandresgp.com" target="_blank" rel="noopener noreferrer">
+    Javier Andrés GP
+  </a>
+  <span class="nav-link disabled " tabindex="-1">&nbsp;&nbsp;Found a bug or have an idea? Contact me</span>
+</nav>
   `;
   return $header;
 }
